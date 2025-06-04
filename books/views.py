19b3ca69a71f0e14d1ca7a5d465c0338d2abda06
Oleksandr Kurin <profysss8712@gmail.com>
2025-06-04 19:00:41 +0300

    Add BookViewSet with CRUD operations

--- books/views.py
+++ books/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+
+class BookViewSet(viewsets.ModelViewSet):
+    """CRUD API for books."""
+
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [AllowAny]
+
