39393379efd37ad45ee9262492c03c4b3ef73c47
Oleksandr Kurin <profysss8712@gmail.com>
2025-06-04 20:20:50 +0300

    Add list/retrieve views for borrowings

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Borrowing
+from .serializers import BorrowingReadSerializer
 
-# Create your views here.
+
+class BorrowingViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    ViewSet for listing and retrieving Borrowings.
+    Only authenticated users can view their borrowings.
+    Admins see all.
+    """
+    queryset = Borrowing.objects.select_related("user", "book").all()
+    serializer_class = BorrowingReadSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff:
+            return self.queryset
+        return self.queryset.filter(user=user)
+
