c37af34726fac7c12c8d4f83eb735a1949c80804
Oleksandr Kurin <profysss8712@gmail.com>
2025-06-04 19:25:09 +0300

    Create IsAdminOrReadOnly permission class

--- library_service_api/urls.py
+++ library_service_api/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 
 # from django.contrib import admin
 from django.urls import path
-
+from django.contrib import admin
 urlpatterns = [
        path("admin/", admin.site.urls),
        path("api/", include("books.urls")),
