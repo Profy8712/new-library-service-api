84e813c12fbdda3f1dd936a472fec986def33889
Oleksandr Kurin <profysss8712@gmail.com>
2025-06-04 20:31:04 +0300

    Implement inventory validation & Create borrowing endpoint with auto user assignment

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,17 +1,23 @@
 from rest_framework import viewsets, permissions
+from rest_framework.response import Response
+from rest_framework import status
+
+from . import serializers
 from .models import Borrowing
-from .serializers import BorrowingReadSerializer
+from .serializers import BorrowingReadSerializer, BorrowingCreateSerializer
 
 
-class BorrowingViewSet(viewsets.ReadOnlyModelViewSet):
+class BorrowingViewSet(viewsets.ModelViewSet):
     """
-    ViewSet for listing and retrieving Borrowings.
-    Only authenticated users can view their borrowings.
-    Admins see all.
+    Borrowing CRUD: read/list for all, create for users.
     """
     queryset = Borrowing.objects.select_related("user", "book").all()
-    serializer_class = BorrowingReadSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    def get_serializer_class(self):
+        if self.action == "create":
+            return BorrowingCreateSerializer
+        return BorrowingReadSerializer
 
     def get_queryset(self):
         user = self.request.user
@@ -19,3 +25,11 @@ class BorrowingViewSet(viewsets.ReadOnlyModelViewSet):
             return self.queryset
         return self.queryset.filter(user=user)
 
+    def perform_create(self, serializer):
+        book = serializer.validated_data["book"]
+        # inventory validation (in addition to serializer)
+        if book.inventory < 1:
+            raise serializers.ValidationError("Book is out of stock.")
+        book.inventory -= 1
+        book.save()
+        serializer.save(user=self.request.user)
